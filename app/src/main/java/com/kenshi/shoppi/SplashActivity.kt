1d7f4ad018397a5e77fe589006cd5497e0fe9d2d
easyhooon <51016231+easyhooon@users.noreply.github.com>
2022-03-30 15:46:02 +0900

    안드로이드 버전별 스플래시 화면 구현

---
+++ app/src/main/java/com/kenshi/shoppi/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.kenshi.shoppi
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.window.SplashScreen
+
+//layout 파일을 통해 layout 을 만드는게 아닌 drawable 파일로
+//30이하의 버전에서 splash 화면을 만들기 위해 SplashActivity 를 필요로 함
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
